0c5053ec15b4b83782d11ae41b5e2b6a83610ec7
kamoojan <31631736+kamoojan@users.noreply.github.com>
2019-12-04 22:16:30 -0800

    Add files via upload
    Uploading pseudocode version

---
+++ main.py
@@ -0,0 +1,50 @@
+wait(10)
+
+class Player:
+  x = middle of window
+  y = middle of window
+  pdirection = up
+  score = 0
+
+class Blast:
+  damage = 1
+  spawn.x == player.x
+  spawn.y == player.y
+  bdirection == mouse.Position
+
+class Enemy4:
+  health = 3
+
+
+class Enemy3:
+  health = 5
+
+class Enemy2:
+  health = 7
+
+class Enemy1:
+  health = 15
+
+while True:
+  if space = pressed 
+    create: Bullet 
+  if Bullet.Position == Enemy.Position
+    then enemy.health -= 1
+    score += 1
+  if enemy.health <= 0
+    then destroy(enemy)
+
+while True:
+  Create:(Randomizer to pick enemy1-4):Position(randomizer for anywhere in the window)
+  wait(10)
+
+wait(2 and a half minutes)
+
+Print("You got "+score+" points!")
+Prompt("play again?")
+if Prompt == Yes
+  then restart program
+
+
+
+
